01d65bee498f923fa8138d106b14e63837f88428
Noemi Puskas <amifontos@gmail.com>
2021-03-21 00:41:10 +0100

    Sidebar: Reduced line height at checkboxes with hide-details

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,13 +2,28 @@
   <div>
     <p>Alcohol content</p>
     <v-checkbox
-      v-for="(percent, index) in alcohol"
+      hide-details
+      v-for="(alcohol, index) in alcoholLevels"
       :key="index"
-      :label="percent.text"
-      v-model="percent.value"
-    ></v-checkbox>
+      :label="alcohol.text"
+      v-model="alcohol.value"
+    />
     <p>Bitterness</p>
+    <v-checkbox
+      hide-details
+      v-for="(bitterness, index) in bitternessLevels"
+      :key="index"
+      :label="bitterness.text"
+      v-model="bitterness.value"
+    />
     <p>Color</p>
+    <v-checkbox
+      hide-details
+      v-for="(color, index) in colors"
+      :key="index"
+      :label="color.text"
+      v-model="color.value"
+    />
   </div>
 </template>
 
@@ -17,18 +32,18 @@ export default {
   name: "Sidebar",
 
   data: () => ({
-    alcohol: [
+    alcoholLevels: [
       { text: "non-alcoholic - 0%", value: false },
       { text: "low - 0-4%", value: false },
       { text: "medium - 4-8%", value: false },
       { text: "high - >8%", value: false },
     ],
-    bitterness: [
+    bitternessLevels: [
       { text: "low - IBU 0-40", value: false },
       { text: "medium - IBU 40-80", value: false },
       { text: "high IBU 80-120", value: false },
     ],
-    color: [
+    colors: [
       { text: "light - EBC 0-20", value: false },
       { text: "medium - EBC 20-40", value: false },
       { text: "dark - EBC 40-80", value: false },
